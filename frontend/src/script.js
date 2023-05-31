cd036caea588b4e078b004e123729795dff4feff
GeorgesB <georgesballister.profissional@gmail.com>
2023-05-31 17:33:32 -0300

    ATT

---
+++ frontend/src/script.js
@@ -0,0 +1,150 @@
+const cafeteria = require("../../model/cafeteria");
+
+button.addEventListener("click", function () {
+  button.style.backgroundColor = "brown";
+});
+
+var menuItems = document.querySelectorAll('.footer-menu ul li');
+
+// for (var i = 0; i < menuItems.length; i++) {
+// menuItems[i].addEventListener('click', function() {
+//     for (var j = 0; j < menuItems.length; j++) {
+//     menuItems[j].classList.remove('active');
+//     }
+//     this.classList.add('active');
+// });
+// }
+
+function toggleActive(button) {
+  // Get the button's ID
+  var buttonId = button.id;
+
+  // Get the grid number from the button's ID
+  var gridNumber = buttonId.substring(3); // Assuming the button IDs are in the format "btnX"
+
+  // Toggle off all grid divs
+  var gridDivs = document.getElementsByClassName("cafeteria-container")[0].children;
+  for (var i = 0; i < gridDivs.length; i++) {
+    gridDivs[i].style.display = "none";
+  }
+
+  // Show the selected grid div if it's not already visible
+  var selectedGridDiv = document.getElementById("grid-" + gridNumber);
+  if (selectedGridDiv.style.display !== "block") {
+    selectedGridDiv.style.display = "block";
+  }
+}
+
+/*function toggleActive(btn) {
+    // remove active class from all buttons
+    const buttons = document.querySelectorAll('.btn-group-vertical button');
+    buttons.forEach(button => {
+      button.classList.remove('active');
+    });
+  
+    // add active class to clicked button
+    btn.classList.add('active');
+  
+    // hide all grids except the one related to the clicked button
+    const grids = document.querySelectorAll('.row.overflow-x-auto');
+    grids.forEach(grid => {
+      if (grid.id === 'grid' + btn.id.slice(-1)) {
+        grid.style.display = 'grid';
+      } else {
+        grid.style.display = 'none';
+      }
+    });
+    
+    // hide base grid
+    const base = document.getElementById("grid0");
+    base.style.display = 'none';
+  
+    // hide text for recommended cafes
+    const rec = document.getElementById("txtrec");
+    rec.style.display = 'none';
+  
+    // show text for most evaluated cafes
+    const ava = document.getElementById("txtava");
+    ava.style.display = 'flex';
+}*/
+
+function ocultarElementos() {
+  var conteudo = document.getElementById("conteúdo");
+  var resultados = document.getElementById("search-result");
+  var search = document.getElementById("search1");
+  conteudo.style.display = 'none';
+  fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Search the JSON file on keyup event
+    search.addEventListener('keyup', () => {
+      // Get the user's input
+      const input = search.value.toLowerCase();
+
+      // Only show resultados after at least 3 characters have been entered
+      if (input.length < 3) {
+        resultados.style.display = 'none';
+        return;
+      }
+
+      // Filter the data array based on the user's input
+      const filteredData = data.filter(item => {
+        return item.nome.toLowerCase().includes(input) || item.local.toLowerCase().includes(input);
+      });
+
+      // Clear the resultados container
+      resultados.innerHTML = '';
+
+      // Display the filtered resultados in the resultados container
+      /*filteredData.forEach(item => {
+        const listapesq = document.createElement('li');
+        listapesq.innerText = `${item.nome}`;
+        listapesq.setAttribute('data-href', `https://google.com`);
+        resultados.appendChild(listapesq);
+      });*/
+      filteredData.forEach((item, index) => {
+        if (index >= 10) {
+          return; // sai do loop após o 10º item
+        }
+        const listapesq = document.createElement('li');
+        const link = document.createElement('a');
+      
+        link.innerText = item.nome;
+        link.href = item.links;
+        listapesq.appendChild(link);
+        resultados.appendChild(listapesq);
+      
+        listapesq.addEventListener('click', function() {
+          window.location.href = item.links;
+        });
+      });
+
+      // Show the resultados container
+      resultados.style.display = 'block';
+    });
+  });
+}
+
+function exibirElementos() {
+  var conteudo = document.getElementById("conteúdo");
+  var resultados = document.getElementById("search-result");
+  resultados.addEventListener('click', function(event) {
+    if (event.target.tagName != 'A') {
+      conteudo.style.display = 'block';
+      resultados.style.display = 'none';
+      return;
+    }
+    /*if (event.target.tagName === 'A') {
+      return;
+    }else{
+      conteudo.style.display = 'block';
+      resultados.style.display = 'none';
+    }
+    conteudo.style.display = 'block';
+    resultados.style.display = 'none';*/
+    
+  });
+}
+
+  
+
